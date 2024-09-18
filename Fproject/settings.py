8204fc9206c4ff3d7a92380fae553aa5d658f5a5
shanmuga-priyaa <appshanmugapriyaa@gmail.com>
2024-09-18 12:22:40 +0530

    fristversion

--- Fproject/settings.py
+++ Fproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'authentication.User_details'
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'static')
+]
